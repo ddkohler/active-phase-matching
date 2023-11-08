d98494b0e9ecfb720e3a4b4517f2ecfacd9e9fe7
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-11-08 12:26:17 -0600

    Update fig4 - correction assessments.py
    each pcolor is individually normalized to max scale

--- figures/fig4 - correction assessments.py
+++ figures/fig4 - correction assessments.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 here = pathlib.Path(__file__).resolve().parent
-save = True
+save = False
 
 exp = wt.open(here.parent / "data" / "composed.wt5").processed
 exp.smooth(2, channel="diff")
@@ -39,13 +39,20 @@ exp.create_channel("diff_max", exp.diff[:].max(axis=(2,3)).reshape(exp.pm_max.sh
 d_empirical = exp.at(L3_points=[0,"ps"], L1_points=[0, "ps"])  # coords do not matter
 d_empirical.transform("w1_points", "w2_points")
 
-common_mag1 = max(d_boxcars.diff[:].max(), d_empirical.diff_max[:].max())
-ax0.pcolormesh(d_boxcars, channel="diff", vmax=common_mag1)
-ax1.pcolormesh(d_empirical, channel="diff_max", vmax=common_mag1)
+if False:
+    common_mag1 = max(d_boxcars.diff[:].max(), d_empirical.diff_max[:].max())
+    ax0.pcolormesh(d_boxcars, channel="diff", vmax=common_mag1)
+    ax1.pcolormesh(d_empirical, channel="diff_max", vmax=common_mag1)
 
-common_mag2 = max(d_boxcars.diff_norm[:].max(), d_empirical.pm_max[:].max())
-ax2.pcolormesh(d_boxcars, channel="diff_norm", vmax=common_mag2)
-ax3.pcolormesh(d_empirical, channel="pm_max", vmax=common_mag2)
+    common_mag2 = max(d_boxcars.diff_norm[:].max(), d_empirical.pm_max[:].max())
+    ax2.pcolormesh(d_boxcars, channel="diff_norm", vmax=common_mag2)
+    ax3.pcolormesh(d_empirical, channel="pm_max", vmax=common_mag2)
+else:
+    ax0.pcolormesh(d_boxcars, channel="diff")
+    ax1.pcolormesh(d_empirical, channel="diff_max")
+    ax2.pcolormesh(d_boxcars, channel="diff_norm")
+    ax3.pcolormesh(d_empirical, channel="pm_max")
+
 
 for ax in [ax0, ax2]:
     wt.artists.set_ax_labels(ax, ylabel=r"$\bar{\nu}_2 \ (\mathsf{cm}^{-1})$")
@@ -60,4 +67,4 @@ for i, ax in enumerate(fig.axes):
 if not save:
     plt.show()
 else:
-    wt.artists.savefig(here / f"{pathlib.Path(__file__).name}.png")
+    wt.artists.savefig(here / f"{pathlib.Path(__file__).name[:-3]}.png")
